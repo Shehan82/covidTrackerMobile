5fc3c024992c21485ec2894d4c87f8017abd8a25
shehan82 <shehansandeepa82@gmail.com>
2020-12-29 20:39:43 +0530

    create appNavigator.js

---
+++ app/navigations/AppNavigator.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import DeathsScreen from "../screens/DeathsScreen";
+import RecoveriesScreen from "../screens/RecoveriesScreen";
+import CasesScreen from "../screens/CasesScreen";
+const Tab = createBottomTabNavigator();
+
+const AppNavigator = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="recovery" component={RecoveriesScreen} />
+    <Tab.Screen name="deaths" component={DeathsScreen} />
+    <Tab.Screen name="cases" component={CasesScreen} />
+  </Tab.Navigator>
+);
+
+export default AppNavigator;
