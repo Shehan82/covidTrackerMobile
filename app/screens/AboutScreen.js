4ee9ea9b4a11c490c86985af085ba0a7aa7983ce
shehan82 <shehansandeepa82@gmail.com>
2021-01-03 09:53:22 +0530

    create about screen

---
+++ app/screens/AboutScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const AboutScreen = () => {
+  return (
+    <View>
+      <Text>This is about</Text>
+    </View>
+  );
+};
+
+export default AboutScreen;
+
+const styles = StyleSheet.create({});
--- app/screens/DeathsScreen.js
+++ app/screens/DeathsScreen.js
@@ -129,6 +129,6 @@ const styles = StyleSheet.create({
   txt: {
     fontSize: 16,
     fontFamily: Platform.OS === "ios" ? "Avenir" : "Roboto",
-    fontWeight: "bold",
+    // fontWeight: "bold",
   },
 });
